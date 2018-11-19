bc2b6cb39b66f7c0c82d384956bd391d36b3a07e
ichicoro <ichi@ichico.ro>
2018-11-19 23:54:55 +0100

    Fix prettyprinting function and update default plugin_list

--- utils.py
+++ utils.py
@@ -2,13 +2,13 @@ import threading, json
 
 # Pretty-prints a string with equal signs
 def pprint(s):
-    print(f"{len(s)}\n{s}\n{len(s)}")
+    print(f"{"="*len(s)}\n{s}\n{"="*len(s)}")
 
 
 def generate_config_file():
     sample_settings = {
         "TELEGRAM_API_TOKEN": "",
-        "plugin_list": [ "hello_plugin" ],
+        "plugin_list": [ "hello_plugin", "rule34", "lyrics" ],
         "plugin_settings": []
     }
     with open('config.json', 'w') as file:
